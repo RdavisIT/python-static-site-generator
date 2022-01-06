5effe9c3b4246da2b04057c0ae1fb712b7b189c1
RdavisIT <RdavisIT8@gmail.com>
2022-01-06 15:16:32 -0700

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -1,23 +1,11 @@
-<<<<<<< Updated upstream
 from pathlib import Path
-=======
-from pathlib import path
->>>>>>> Stashed changes
 
 
 class Site:
 
     def __init__(self, source, dest):
-<<<<<<< Updated upstream
         self.source = Path(source)
         self.dest = Path(dest)
 
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
-=======
-        self.source = path(source)
-        self.dest = path(dest)
-
-    def create_dir(self, path):
-        directory = self.dest
->>>>>>> Stashed changes
