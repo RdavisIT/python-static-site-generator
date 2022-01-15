36d3ea55e397c3e08c2c2ee37c7657473ed41013
RdavisIT <RdavisIT8@gmail.com>
2022-01-15 12:57:53 -0700

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -22,4 +22,4 @@ class Parser:
             file.write(content)
 
     def copy(self, path, source, dest):
-        shutil.copy2(dest / path.relative_to(source), dest)
+        shutil.copy2(path, dest / path.relative_to(source))
