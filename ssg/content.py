19da2a650588178bac4e931f6edcc2bd8a86083e
RdavisIT <RdavisIT8@gmail.com>
2022-02-14 13:04:51 -0700

    Update content.py

--- ssg/content.py
+++ ssg/content.py
@@ -10,5 +10,5 @@ class Content(Mapping):
     @classmethod
     def load(cls, string):
         _, fm, content = cls.__regex.split(string, 2)
-        load(fm, Loader=FullLoader)
+        metadata = load(fm, Loader=FullLoader)
         return cls(metadata, content)
